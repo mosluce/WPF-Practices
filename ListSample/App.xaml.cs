5f4d3909f3d73d7aee0f30a086585de0fa3b8d99
mosluce <mosluce@gmail.com>
2015-01-26 18:01:12 +0800

    ListSample

---
+++ ListSample/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using ListSample.Data;
+using ListSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ListSample
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            using (var db = new ListSampleDbContext())
+            {
+
+                if (db.Products.Count() == 0) { 
+                    db.Products.Add(new Product
+                    {
+                        Name = "牛奶",
+                        Price = 500
+                    });
+
+                    db.Products.Add(new Product
+                    {
+                        Name = "可樂",
+                        Price = 30
+                    });
+
+                    db.Products.Add(new Product
+                    {
+                        Name = "麵包",
+                        Price = 25
+                    });
+
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
---
+++ ListSample/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using ListSample.Data;
+using ListSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListSample
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ListSampleDbContext _dbContext { get; set; }
+        public ObservableCollection<Product> ProductItems { get; set; }
+        public MainWindow()
+        {
+            _dbContext = new ListSampleDbContext();
+            
+            InitializeComponent();
+        }
+
+        private void ListSampleWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ProductItems = new ObservableCollection<Product>(_dbContext.Products.ToList());
+            ProductListView.ItemsSource = ProductItems;
+        }
+    }
+}
